50f5cb66a3647a7bc74fa4b2064c895c9f2b81b7
haneul <ver.753@gmail.com>
2020-08-28 01:44:14 +0900

    Receive data in while loop

--- socket/server_win.py
+++ socket/server_win.py
@@ -1,15 +1,24 @@
 from socket import *
+import cv2 as cv
 
+address = '127.0.0.1'
+port = 8090
 server_sock = socket(AF_INET, SOCK_STREAM)
-server_sock.bind(('127.0.0.1', 8081))
+server_sock.bind((address, port))
+print('bind success')
+print('listening...')
 server_sock.listen(1)
 
 connection_sock, addr = server_sock.accept()
 
 print(str(addr), 'tried to connect. check connection.')
 
-data = connection_sock.recv(1024)
-print('received data: ', data)
+while True:
+    #key = cv.waitKey(1)
+    data = connection_sock.recv(1024)
+    #if key == 27:
+    #    break
+    print('received data: ', data)
 
-connection_sock.send('I am a server.')
-print('send message')
+connection_sock.send('server close the socket.')
+print('close connection')
