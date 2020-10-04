7f2fbd13e476288801501d134c756d243ede3ad8
Ha-neul Bae <ver.753@gmail.com>
2020-10-04 23:59:32 +0100

    Modify camera code

--- test_main.py
+++ test_main.py
@@ -1,23 +1,24 @@
-# from https://webnautes.tistory.com
-import cv2 as cv # opencv __version__ 4.2.0
+import cv2 as cv # opencv __version__ 4.1.1
 from socket import *
 
-# parameter 0 means main web camera
+# parameter 0 means main pi camera
 cap = cv.VideoCapture(0)
 
-list_ball_location = []
-history_ball_locations = []
 isDraw = False # Do not draw as soon as the program starts
 
 # make socket
+addr = '192.168.35.176'
+port = 8090
 client_sock = socket(AF_INET, SOCK_STREAM)
-client_sock.connect(('192.168.35.176', 8090))
+client_sock.connect((addr, port))
 
 print('connection success')
-client_sock.send(str('client: connection is okay').encode())
+#client_sock.send(str('client: connection is okay').encode())
+if not cap.isOpened():
+    cap.open()
 
 # __main()__
-while cap.isOpened():
+while True:
     # loading video
     ret,img_color = cap.read()
 
@@ -58,26 +59,32 @@ while cap.isOpened():
         # make a circle and square
         center_x = int(centroids[max_index, 0]) 
         center_y = int(centroids[max_index, 1])
-        left = stats[max_index, cv.CC_STAT_LEFT]
-        top = stats[max_index, cv.CC_STAT_TOP]
-        width = stats[max_index, cv.CC_STAT_WIDTH]
-        height = stats[max_index, cv.CC_STAT_HEIGHT]
+        #left = stats[max_index, cv.CC_STAT_LEFT]
+        #top = stats[max_index, cv.CC_STAT_TOP]
+        #width = stats[max_index, cv.CC_STAT_WIDTH]
+        #height = stats[max_index, cv.CC_STAT_HEIGHT]
 
-        cv.rectangle(img_color, (left, top), (left + width, top + height), (0, 0, 255), 5)
+        #cv.rectangle(img_color, (left, top), (left + width, top + height), (0, 0, 255), 5)
         cv.circle(img_color, (center_x, center_y), 10, (0, 255, 0), -1)
 
-        client_sock.send((str(center_x)+','+str(center_y)).encode())
+        # send coordinate x and y
+        client_sock.send((str(center_x)+','+str(center_y)+']').encode())
 
         # create window and show the result
         cv.imshow('Binarization', img_mask)
         cv.imshow('Result', img_color)
 
-        # Using keyboard function for drawing or not or clear all
+        # Using keyboard function for break
         key = cv.waitKey(1)
         if key == 27: # esc
+            client_sock.send('quit'.encode())
             break
 
-print('main() is ended')
-data = client_sock.recv(1024)
-print('received data:', data.decode())
+cv.destroyAllWindows()
+print('system: main() is ended')
+buf = client_sock.recv(1024)
+print('serv msg: ', buf.decode());
+print('system: Close client socket')
 client_sock.close()
+print('bye')
+
