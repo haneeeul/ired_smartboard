ba0fcf184aa2ac0b9305b4490d321c844edd1d2a
Haneul <ver753@gmail.com>
2020-10-05 10:36:36 +0900

    version 1

--- socket/server_win.py
+++ socket/server_win.py
@@ -15,29 +15,31 @@ connection_sock, addr = server_sock.accept()
 print(str(addr), 'tried to connect. check connection.')
 
 coordinate = []
+File = open("./log.txt", mode='w', encoding='utf-8')
+
 
 while True:
-	
-	data = connection_sock.recv(1024)
-	# data decoding
-	# byte to str
-	
-	data = data.decode('utf-8')
-	# check quit sign
-	if data == 'quit':
-		break
-	coordinate = data.split(']')
-	for val in coordinate:
-	    if len(val) <= 1:
-	        continue
-	    center = val.split(',')
-	    center_x = int(center[0])
-	    center_y = int(center[1])
-	    print('the coordinate is: ', center_x, end=' ')
-	    print(', ', center_y)
-	    #time.sleep(1)
-		
+    
+    data = connection_sock.recv(1024)
+    # data decoding
+    # byte to str
+    
+    data = data.decode('utf-8')
+    # check quit sign
+    if data == 'quit':
+        break
+    coordinate = data.split(']')
+    for val in coordinate:
+        if len(val) <= 1:
+            continue
+        center = val.split(',')
+        center_x = int(center[0])
+        center_y = int(center[1])
+        fdata = "%d" % center_x
+        fdata = fdata + ', ' + "%d" % center_y
+        File.write(fdata + '\n')
 
+File.close()
 connection_sock.send('server close the socket.'.encode())
 connection_sock.close()
 server_sock.close()
