960c390aa03ce41c91f3f1e74b57e92a0b467922
Haneul Bae <ver753@gmail.com>
2020-10-19 00:44:22 +0900

    Modify code style and removing String_processing function

--- socket/server_win.py
+++ socket/server_win.py
@@ -4,55 +4,60 @@ import numpy as np
 import pyautogui
 #import time
 def draw_ball_location(img_color, locations):
-    '''
-	for i in range(len(locations) - 1):
+    print("Enter the function")
+    for i in range(len(locations) - 1):
         # if locations is empty
         if locations[0] is None or locations[1] is None:
             continue
-    '''
         # draw each coordinates in locations on window
         # BGR of yellow = (0, 255, 255)
-
-    cv.line(img_color, tuple(locations[0]), tuple(locations[1]), (255, 255, 255), 3)
+        print("draw something")
+        cv.line(img_color, tuple(locations[i]), tuple(locations[i+1]), (0, 255, 255), 3)
 
 
 def Bind_n_listen(addr, port_number):
-	sock = socket(AF_INET, SOCK_STREAM)
-	sock.bind((addr, port_number))
-	print("binding success!")
-	print("listening...")
-	sock.listen(1)
-	return sock
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind((addr, port_number))
+    print("binding success!")
+    print("listening...")
+    sock.listen(1)
+    return sock
 
 def Connect_to_client(sock):
-	conn, addr = sock.accept()
-	print(str(addr), "tried to connect.")
-	return conn
+    conn, addr = sock.accept()
+    print(str(addr), "tried to connect.")
+    return conn
 
 def String_processing(sdata):
+    # list values to str
+    string = ''.join(sdata)
     
-	if 'quit' in sdata:
-	    return tuple(-1, -1)
-	
-	coor = sdata.split(']')
+    print(string)
+    print(type(string))
+
+    if 'quit' in string:
+        return -1, -1
     
-	if len(sdata) <= 1:
-	    continue
-        
-    center = val.split(',')
+    coor = string.split(']')
+    '''
+    if len(coor) <= 1:
+    '''    
+     
+    string_coor = ''.join(coor)
+    center = string_coor.split(',')
     center_x = int(center[0])
     center_y = int(center[1])
     print("center_x, center_y", center_x, center_y)
     print("\n")
 
-	return tuple(center_x, center_y)
+    return center_x, center_y
         
 
 address = '0.0.0.0'
 port = 8090
 
 server_sock = Bind_n_listen(address, port)
-client_sock = Connect_to_client(server_sock)
+conn_sock = Connect_to_client(server_sock)
 
 coordinate = []
 list_ball_location = []
@@ -65,25 +70,46 @@ cv.namedWindow("Window")
 cv.moveWindow("Window", 960, 0)
 
 while True:
+    '''
     # take a screenshot using base picture
     pic = pyautogui.screenshot(region=(0, 0, 1000, 960))
     #pic = pyautogui.screenshot()
     img_frame = np.array(pic)
     img_frame = cv.cvtColor(img_frame, cv.COLOR_RGB2BGR)
-    
+    '''
     while True:
-	    data = client_sock.recv(1024)
-	    if not data: break
-	    # data decoding: byte to str
-	    data = data.decode('utf-8')
-	    center_x, center_y = String_processing(data)
         
-	    # write on log file
+        # take a screenshot using base picture
+        pic = pyautogui.screenshot(region=(0, 0, 1000, 960))
+        #pic = pyautogui.screenshot()
+        img_frame = np.array(pic)
+        img_frame = cv.cvtColor(img_frame, cv.COLOR_RGB2BGR)
+        
+        data = conn_sock.recv(1024)
+        if not data: break
+        # data decoding: byte to str
+        data = data.decode('utf-8')
+        if 'quit' in data: break
+        coor = data.split(']')
+        for val in coor:
+            if len(val) <= 1:
+                continue
+            center = val.split(',')
+            center_x = int(center[0])
+            center_y = int(center[1])
+            list_ball_location.append((center_x, center_y))
+        '''
+        center_x, center_y = String_processing(data)
+        
+        if center_x == -1 and  center_y == -1:
+            break
+        '''
+        # write on log file
         fdata = "%d" % center_x
         fdata = fdata + ', ' + "%d" % center_y
         File.write(fdata + '\n')
-       
-        list_ball_location.append((center_x, center_y))
+        
+        #list_ball_location.append((center_x, center_y))
 
         '''
         if isDraw:
@@ -97,8 +123,6 @@ while True:
         for ball_locations in history_ball_locations:
             draw_ball_location(img_frame, ball_locations)
         '''
-		if len(list_ball_location) <= 2:
-		    continue
         draw_ball_location(img_frame, list_ball_location)
         cv.imshow('Window', img_frame)
 
@@ -107,12 +131,12 @@ while True:
             break
 
     File.close()
-	print("log file closed.")
-	print("send closing msg to client.")
+    print("log file closed.")
+    print("send closing msg to client.")
     conn_sock.send('server close the socket.'.encode())
     conn_sock.close()
     server_sock.close()
 
     print('close ALL connection')
-	break
+    break
 
